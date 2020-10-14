e299d92eb2a6e2d427075a35bb0d0f4d0ed29294
MAKUSref <tomaszmakowski002731@gmail.com>
2020-10-14 17:06:11 +0200

    add webpack config

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import HomeworkListing from './js/homeworkListing';
+import 'normalize.css/normalize.css';
+import "./scss/style.scss";
+import "bootstrap";
+import '@fortawesome/fontawesome-free/js/all';
+
+const instance = new HomeworkListing();
---
+++ src/js/homeworkListing.js
@@ -0,0 +1,7 @@
+class HomeworkListing {
+  constructor() {
+    console.log('homework listing');
+  }
+}
+
+export default HomeworkListing;
